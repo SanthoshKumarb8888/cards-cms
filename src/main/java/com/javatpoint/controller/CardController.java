6cd46cc2b538c036152ca4c7349f366c3499f358
Santhosh Kumar <111180906+SanthoshKumarb8888@users.noreply.github.com>
2024-06-15 18:34:04 +0530

    Update CardController.java

--- src/main/java/com/javatpoint/controller/CardController.java
+++ src/main/java/com/javatpoint/controller/CardController.java
@@ -1,8 +1,11 @@
 package com.javatpoint.controller;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,16 +28,24 @@ public String healthCheck()
 return "Your application is up and running";
 }
 
-@GetMapping("/getAllCardsDetails")
+
+ private Map<Long, Card> cardMap = new HashMap<>();
+
+    // Initialize sample static data
+    public CardController() {
+        cardMap.put(1L, new Card(1L, "Card 1", "Description of Card 1"));
+        cardMap.put(2L, new Card(2L, "Card 2", "Description of Card 2"));
+        cardMap.put(3L, new Card(3L, "Card 3", "Description of Card 3"));
+    }
+
+    @GetMapping("/getAllCardsDetails")
     public List<Card> getAllCards() {
-        List<Card> cards = new ArrayList<>();
-        
-        // Sample static data
-        cards.add(new Card(1L, "Card 1", "Description of Card 1"));
-        cards.add(new Card(2L, "Card 2", "Description of Card 2"));
-        cards.add(new Card(3L, "Card 3", "Description of Card 3"));
-        
-        return cards;
+        return new ArrayList<>(cardMap.values());
+    }
+
+    @GetMapping("/getCardDetails/{cardId}")
+    public Card getCardDetails(@PathVariable Long cardId) {
+        return cardMap.get(cardId);
     }
 
   
